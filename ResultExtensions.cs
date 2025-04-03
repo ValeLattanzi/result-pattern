654704e646f020184091ec1358ecc1709c59c492
valentino <vale.lattanzi15@gmail.com>
2025-04-03 16:23:53 -0300

    fix: error with old dependencies

--- ResultExtensions.cs
+++ ResultExtensions.cs
@@ -1,25 +1,25 @@
 ﻿namespace result_pattern;
 
 public static class ResultExtensions {
-  public static TOut Match<TOut>(
+  public static TOut match<TOut>(
     this Result result,
     Func<TOut> onSuccess,
     Func<Result, TOut> onFailure) {
     return result.IsSuccess ? onSuccess() : onFailure(result);
   }
 
-  public static TOut Match<TIn, TOut>(
+  public static TOut match<TIn, TOut>(
     this Result<TIn> result,
     Func<TIn, TOut> onSuccess,
     Func<Result<TIn>, TOut> onFailure) {
     return result.IsSuccess ? onSuccess(result.Value) : onFailure(result);
   }
 
-  public static Result<TOut> Bind<TIn, TOut>(this Result<TIn> result, Func<TIn, Result<TOut>> bind) {
+  public static Result<TOut> bind<TIn, TOut>(this Result<TIn> result, Func<TIn, Result<TOut>> bind) {
     return result.IsSuccess ? bind(result.Value) : Result.failure<TOut>(result.Error);
   }
 
-  public static Result<TOut> TryCatch<TIn, TOut>(this Result<TIn> result, Func<TIn, TOut> func, Error error) {
+  public static Result<TOut> tryCatch<TIn, TOut>(this Result<TIn> result, Func<TIn, TOut> func, Error error) {
     try {
       return result.IsSuccess ? Result.success(func(result.Value)) : Result.failure<TOut>(result.Error);
     }
@@ -28,14 +28,14 @@ public static class ResultExtensions {
     }
   }
 
-  public static Result<TIn> Tap<TIn>(this Result<TIn> result, Action<TIn> action) {
+  public static Result<TIn> tap<TIn>(this Result<TIn> result, Action<TIn> action) {
     if (result.IsSuccess)
       action(result.Value);
 
     return result;
   }
 
-  public static ObjectResult ToProblemDetails(this Result result) {
-    return new(ApiResults.problem(result));
+  public static object toProblemDetails(this Result result) {
+    return ApiResults.problem(result);
   }
 }
